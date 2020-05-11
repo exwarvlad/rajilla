1d0650ef300abd3f7360e9d4004c582865827171
exwarvlad <exwarvlad@gmail.com>
2020-05-11 18:40:23 +0300

    added models validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   has_many :tasks
+
+  validates :name, presence: true
+  validates :price, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,30 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :urls, presence: true
+  validates :progress, inclusion: { in: 0..100, allow_blank: true }
+
+  validate :date_biggest_or_eq_curr
+  validate :price_less_or_eq_project_price
+  validate :scan_urls
+
+  enum status: %i[initialized proccesing failed finished]
+
+  private
+
+  def date_biggest_or_eq_curr
+    return unless estimate_date
+
+    errors.add(:estimate_date, "can't be less then current date") if proc { estimate_date < Time.zone.today }
+  end
+
+  def price_less_or_eq_project_price
+    return unless price
+
+    errors.add(:price, "can't be biggest then project price") if price.to_f > project.price.to_f
+  end
+
+  def scan_urls
+    errors.add(:price, "wrong urls") unless urls.all? { |url| url =~ URI::regexp }
+  end
 end
